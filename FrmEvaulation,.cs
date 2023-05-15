75928d90cca0958124366d3d9ba1cc82e9b7cd61
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-15 20:19:15 +0200

    evaulation fm

--- FrmEvaulation,.cs
+++ FrmEvaulation,.cs
@@ -13,6 +13,9 @@ using Evaluation_Manager.Repositories;
 namespace Evaluation_Manager {
     public partial class FrmEvaulation : Form {
         private Student student;
+
+        public Student SelectedStudent { get=> student; set=> student =value; }
+
         public FrmEvaulation(Student  selectedStudent) {
             InitializeComponent();
             student = selectedStudent;
@@ -36,6 +39,17 @@ namespace Evaluation_Manager {
             txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" + currentActivity.MaxPoints;
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+            var evaulation = EvaulationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if (evaulation != null) {
+                txtTeacher.Text=evaulation.Evaulator.ToString();
+                txtEvaulationDate.Text=evaulation.EvaulationDate.ToString();
+                numPoints.Value = evaulation.Points;
+            } else {
+                txtTeacher.Text=FrmLogin.LoggedTeacher.ToString();
+                txtEvaulationDate.Text = " ";
+                numPoints.Value = 0;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
